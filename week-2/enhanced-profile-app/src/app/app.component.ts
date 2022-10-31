ce60eaa6ebfad7feba5ad0111740c68cedc5c2bc
Kayla McDanel <ks.mcdanel@gmail.com>
2022-10-30 20:22:52 -0400

    Week 2 Data Binding

---
+++ week-2/enhanced-profile-app/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  isLoggedIn: Boolean = true;
+  assignment: string = "Exercise 2.3 - Data Binding";
+}
---
+++ week-2/enhanced-profile-app/src/app/my-details/my-details.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+export default class Person {
+  fullName: string;
+  favoriteFood: string;
+  favoriteColor: string;
+  keywords: ["#TypeScript", "#2022", "#CodingWithAngular", "ngOmaha"];
+
+  constructor(fullName: string, favoriteFood: string, favoriteColor: string) {
+    this.fullName = fullName;
+    this.favoriteFood = favoriteColor;
+    this.favoriteColor = favoriteColor;
+  }
+
+  toString()  {
+    console.log(`\n Full Name: ${this.fullName}\n Favorite Food: ${this.favoriteFood}\n Favorite Color: ${this.favoriteColor}`);
+  }
+}
+
+@Component({
+  selector: 'app-my-details',
+  templateUrl: './my-details.component.html',
+  styleUrls: ['./my-details.component.css']
+})
+export class MyDetailsComponent implements OnInit {
+  myProfile: Person;
+
+  constructor() {
+    this.myProfile = new Person("Kayla McDanel", "Carbs. All of them", "Purple!");
+    this.myProfile.toString();
+  }
+
+  ngOnInit(): void {
+  }
+
+}
